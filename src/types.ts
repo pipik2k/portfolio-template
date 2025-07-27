15cd720a6d9719c684cd957d7c1708546723573f
pipik2k <bts.khanh@gmail.com>
2025-07-27 18:01:02 +0700

    push
    push

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,6 @@ export enum LinkImageSource {
   Github = "/images/logos/github.png",
   ItchIo = "/images/logos/itch.io.png",
   CHPlay = "/images/logos/chplay.png",
-  Zalo = "images/logos/zalo.png"
 }
 
 export enum Platform {
